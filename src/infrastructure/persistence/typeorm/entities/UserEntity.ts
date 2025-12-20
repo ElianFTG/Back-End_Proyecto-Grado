390d40694e59e9b3e97ec4ffc05f6ff9b80807c6
Elian Tito <eliantito234@gmail.com>
2025-12-20 11:23:52 -0400

    asignacion unica de user_name para UserEntity

--- src/infrastructure/persistence/typeorm/entities/UserEntity.ts
+++ src/infrastructure/persistence/typeorm/entities/UserEntity.ts
@@ -24,7 +24,7 @@ export class UserEntity {
   @Column({ type: 'int', nullable: true })
   branch_id!: number | null;
 
-  @Column({ type: 'varchar', length: 50 })
+  @Column({ type: 'varchar', length: 50, unique:true })
   user_name!: string;
   
   @Column({ type: 'varchar', length: 255})
