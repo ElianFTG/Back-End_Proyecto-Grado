d77918258e7682ff543485ea3ddc2b1b52231c46
nicolasvidal03 <nico03vj@gmail.com>
2026-03-04 12:12:16 -0400

    Mas pruebas para railway

--- src/infrastructure/services/NodemailerEmailService.ts
+++ src/infrastructure/services/NodemailerEmailService.ts
@@ -13,6 +13,7 @@ export class NodemailerEmailService implements EmailService {
             port: Number(process.env.SMTP_PORT) || 587,
             secure: false,
             family: 4,
+            requireTLS: true,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASSWORD,
